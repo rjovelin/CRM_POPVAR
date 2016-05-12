a15f15108354f98ea75a43a42b9f26379a3641b5
Richard <richard.jovelin@oicr.on.ca>
2016-05-12 19:01:05 -0400

    changed file path and changed if statement

--- run_make_PB_ON_SNP_files.py
+++ run_make_PB_ON_SNP_files.py
@@ -10,6 +10,10 @@ from parse_vcf_comp_files import *
 from manipulate_sequences import *
 from repeats_TEs import *
 
+
+# use this script to generate SNP files that includes Ontario strains (PX+KSR) and PB strains
+# use these files to compute diversity at non-coding sites
+
 # path to the vcf comp files
 vcf_dir = '/disk3/wei.wang/c_rem_analysis/split_strain_vcf_ran/vcf/'
 
@@ -20,10 +24,9 @@ os.mkdir('../PB_ON_SNP_files/')
 snp_dir = '../PB_ON_SNP_files/'
 
 # create a dict of repeat name coordinate
-repeat_coord = get_repeats_coord('../piRNAs/356_v1_4.fasta.out', False)
+repeat_coord = get_repeats_coord('356_v1_4.fasta.out', False)
 # create a dict of chromo : set of repeat indices
 repeat_pos = get_repeat_positions(repeat_coord)
-
 
 # make a list of filename
 files = [filename for filename in os.listdir(vcf_dir) if 'all' in filename and 'filtered.com' in filename]
@@ -34,13 +37,16 @@ for i in files:
     # get the full path of the file
     filename = vcf_dir + i
     # get chromo and dict with SNP counts
+    # remove SNPs in regions with repeats
     chromo, snps = parse_vcf_comp_PB_ON(filename, repeat_pos, True)
     # check that chromo and snps are defined (ie. that comp file is not empty)
-    if chromo != '' and snps != 0:
+    if chromo != '' and len(snps) != 0:
         # get name of outputfile
         outputfile = snp_dir + 'PBON_' + chromo + '_SNPs.txt'        
         # save SNP info to outputfile
         generate_PB_ON_snp_file(chromo, snps, outputfile)   
+    else:
+        print(i, 'empty')
 
 
 
