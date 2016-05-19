e7bd07bec9340603cf7cd44c73a563fbc5569589
Richard <richard.jovelin@oicr.on.ca>
2016-05-19 15:51:34 -0400

    removed boxplot option

--- run_compare_utr_downstream.py
+++ run_compare_utr_downstream.py
@@ -9,8 +9,6 @@ Created on Thu May 19 14:31:45 2016
 # use this script to plot a graph of nucleotide diversity at miRNA target sites
 # for annotated UTRs and extracted downstream regions
 
-# usage CompareDiversityTargetsUTRNonUTR.py [options]
-# - [box/bar] : plot a bar graph or a boxplot
 
 # use Agg backend on server without X server
 import matplotlib as mpl
@@ -27,10 +25,6 @@ from genomic_coordinates import *
 import numpy as np
 from scipy import stats
 import math
-import sys
-
-# get graph option from command
-graphtype = sys.argv[1]
 
 
 # get the allele counts for all sites with coverage, exclude sites with sample size < 10
@@ -87,78 +81,39 @@ print('targets in UTR: ', len(UTR_targets_theta))
 print('targets in predicted UTR: ', len(nonUTR_targets_theta))
             
 alldata = [nonUTR_targets_theta, UTR_targets_theta]
-site_types = ['Downstream', 'UTR']
+site_types = ['Non-UTR', 'UTR']
 
 
 # create figure
-fig = plt.figure(1, figsize = (2,2))
+fig = plt.figure(1, figsize = (1.5,2))
 # add a plot to figure (1 row, 1 column, 1st plot)
 ax = fig.add_subplot(1, 1, 1)
 
-# check graphich type in option
-if graphtype == 'box':
-    width = 0.8
-    # use a boxplot
-    bp = ax.boxplot(alldata, showmeans = False, showfliers = False, widths = width, patch_artist = True) 
-    # create a list of colors (seee http://colorbrewer2.org/)
-    color_scheme = ['#2ca25f', '#99d8c9']    
-    # color boxes for the different sites
-    i = 0    
-    # change box, whisker color to black
-    for box in bp['boxes']:
-        # change line color
-        box.set(color = 'black', linewidth = 1.5)
-        box.set(facecolor = color_scheme[i])
-        # upate color
-        i += 1
-    # change whisker color ro black
-    for wk in bp['whiskers']:
-        wk.set(color = 'black', linestyle = '-', linewidth = 1.5)
-    # change color of the caps
-    for cap in bp['caps']:
-        cap.set(color = 'black', linewidth = 1.5)
-    # change the color and line width of the medians
-    for median in bp['medians']:
-        median.set(color = 'black', linewidth = 1.5)
+width = 0.5
+ind = np.arange(len(alldata))
+Means = [np.mean(i) for i in alldata]
+SEM = []
+for i in alldata:
+    SEM.append(np.std(i) / math.sqrt(len(i)))
+# use a bar plot
+ax.bar(ind, Means, width, yerr = SEM,
+       color = ['#2ca25f', '#99d8c9'], linewidth = 1.5,
+       error_kw=dict(elinewidth=1.5, ecolor='black', markeredgewidth = 1.5))               
     
-#    # restrict the x and y axis to the range of data
-#    ax.set_ylim([0, 0.07])
-
-
-elif graphtype == 'bar':
-    width = 0.8
-    ind = np.arange(len(alldata))
-    Means = [np.mean(i) for i in alldata]
-    SEM = []
-    for i in alldata:
-        SEM.append(np.std(i) / math.sqrt(len(i)))
-    # use a bar plot
-    ax.bar(ind, Means, width, yerr = SEM,
-           color = ['#8856a7', '#9ebcda', '#e0ecf4'],
-           linewidth = 1.5, error_kw=dict(elinewidth=1.5, ecolor='black', markeredgewidth = 1.5))               
-    
-#    # restrict the x and y axis to the range of data
-#    ax.set_ylim([0, 0.025])
+# restrict the x and y axis to the range of data
+ax.set_ylim([0, 0.030])
 
 # remove lines around the frame
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.spines['left'].set_visible(False)
-if graphtype == 'box':
-    ax.spines['bottom'].set_visible(False)
-elif graphtype == 'bar':
-    ax.spines['bottom'].set_visible(True)
-    # offset the spines
-    for spine in ax.spines.values():
-        spine.set_position(('outward', 5))
+ax.spines['bottom'].set_visible(True)
+# offset the spines
+for spine in ax.spines.values():
+    spine.set_position(('outward', 5))
 
-# set up tick positions and labels
-if graphtype == 'box':
-    # set x tick positions
-    xtickpos = [i + 0.5 for i in range(len(alldata))]
-elif graphtype == 'bar':
-    # set x tick positions
-    xtickpos = [i + width/2 for i in range(len(alldata))]
+# set x tick positions
+xtickpos = [i + width/2 for i in range(len(alldata))]
 ax.set_xticks(xtickpos)
 
 # do not show ticks
@@ -174,10 +129,7 @@ plt.tick_params(
     labelsize = 10,
     direction = 'out')
 
-if graphtype == 'box':
-    plt.xticks(xtickpos, site_types, rotation = 20, ha = 'center', size = 10, fontname = 'Arial')
-elif graphtype == 'bar':
-    plt.xticks(xtickpos, site_types, rotation = 20, ha = 'right', size = 10, fontname = 'Arial')
+plt.xticks(xtickpos, site_types, ha = 'right', size = 10, fontname = 'Arial')
 
 # Set the tick labels font name
 for label in ax.get_yticklabels():
@@ -185,7 +137,7 @@ for label in ax.get_yticklabels():
 
 # write label for x and y axis
 ax.set_ylabel('Nucleotide polymorphism', color = 'black',  size = 10, ha = 'center', fontname = 'Arial')
-ax.set_xlabel('Genomic annotation', color = 'black', size = 10, ha = 'center', fontname = 'Arial')
+ax.set_xlabel('Downstream annotation', color = 'black', size = 10, ha = 'center', fontname = 'Arial')
 
 # add margins
 ax.margins(0.05)
@@ -226,9 +178,5 @@ print(site_types[0], site_types[1], Pval)
 #    ax.text(x_pos[i], y_pos[i], diff[i], horizontalalignment='center',
 #            verticalalignment='center', color = 'black', fontname = 'Arial', size = 10)
 
-if graphtype == 'box':
-    # save figure
-    fig.savefig('DiversityTargetsDownstreamUTR_box.pdf', bbox_inches = 'tight')
-elif graphtype == 'bar':
-    # save figure
-    fig.savefig('DiversityTargetsDownstreamUTR_bar.pdf', bbox_inches = 'tight')
+# save figure
+fig.savefig('DiversityTargetsDownstreamUTR.pdf', bbox_inches = 'tight')
