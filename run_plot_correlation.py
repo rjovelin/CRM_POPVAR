d77fd9daf27e8950008aa226730a52159bd92ca3
Richard <richard.jovelin@oicr.on.ca>
2016-05-19 19:06:03 -0400

    generate scatter plot expression X diversity

--- run_plot_correlation.py
+++ run_plot_correlation.py
@@ -12,142 +12,121 @@ Created on Thu May 19 16:28:06 2016
 
 
 
-
-
-
-
-Name	PX356_scaffold	start	end	orientation	Hairpin	Mature	Seed	hairpin_arm	Emb _average	A _average	B _average	C _average	D _average	E _average	F _average	G _average	remanei_male _average	Emb _stdev	A _stdev	B _stdev	C _stdev	D _stdev	E _stdev	F _stdev	G _stdev	remanei_male _stdev	family_conservation
-
-
-
-
-
-
-
-
-
-
-
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Gist
- @rjovelin
- Unwatch 1
-  Star 0
-  Fork 0 rjovelin/MitoVariants
- Code  Issues 0  Pull requests 0  Wiki  Pulse  Graphs  Settings
-Branch: master Find file Copy path
-MitoVariants/PlotCorrelationReadDepthHeteroplasmies.py
-70061e6  on Mar 24
-@rjovelin rjovelin Initial commit
-1 contributor
-RawBlameHistory     215 lines (162 sloc)  6 KB
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 14 11:47:55 2016
-@author: RJovelin
-"""
-
-# use this script to generate a scatter plot between the number of heteroplasmies
-# and the average or median read depth per individual
-
-# usage PlotCorrelationReadDepthHeteroplasmies.py [options]
-# - ReadDepthFile: file with average and median read depth per cancer and per dataset
-# - HeteroSummaryFile: file with mitoseek output
-# - tumor
-# - [WGS/RNAseq]: use WGS or RNAseq data set
-# - [median/mean]: use median or mean read depth for each individual
-# - output file: save figure file
-
-import os
-import sys
-from scipy import stats
-import numpy as np
+# use Agg backend on server without X server
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
+# load modules
+import numpy as np
+import math
+from scipy import stats
+import sys
+# load custom modules
+from manipulate_sequences import *
+from divergence import *
+from miRNA_target import *
+from sites_with_coverage import *
 
-# get file with median and read depth per individual and cancer and data type
-ReadDepthFile = sys.argv[1]
-# get the summary file with mitoseek output
-HeteroSummaryFile = sys.argv[2]
-# get tumor
-tumor = sys.argv[3]
-# get datatype
-datatype = sys.argv[4]
-# use median or mean read depth
-read_depth = sys.argv[5]
-# save figure to outputfile
-outputfile = sys.argv[6]
 
-# parse file to extract read depth values
-# open file for reading
-infile = open(ReadDepthFile, 'r')
-# skip header
+
+# get option to use expression in males or across development
+expression = sys.argv[1]
+
+
+# create a dict to store the expression level of mature miRNAs
+mature_expression = {}
+# get mature expression from file
+infile = open('CRM_miRNAsCoordinatesFinal.txt')
 infile.readline()
-# create a dict {participant: read_depth}
-ReadDepth = {}
-# loop over file
 for line in infile:
     line = line.rstrip()
     if line != '':
         line = line.split('\t')
-        # get participant
-        participant = line[0]
-        # check if correct tumor and data set 
-        if tumor == line[3] and datatype == line[-1]:
-            # check data type
-            if read_depth == 'mean':
-                data = float(line[1])
-            elif read_depth == 'median':
-                data = float(line[2])
+        mirna = line[0]
+        # check that mirna has expression
+        if len(line) >= 17:
+            # get expression
+            if expression == 'male':
+                # take expression in remanei males
+                level = float(line[17])
+            elif expression == 'development':
+                # take mean of expression at different time points
+                level = np.mean(list(map(lambda x: float(x), line[9:17])))
             # populate dict
-            ReadDepth[participant] = data
-infile.close()
+            mature_expression[mirna] = level
+        else:
+            print('no expression for {0}'.format(mirna))
+print('recorded miR expression')
 
-# create a dict {participant: set(variable positions)}
-Mutations = {}
-# open heteroSummary file for reading
-infile = open(HeteroSummaryFile, 'r')
+
+############################
+
+
+# create a set of valid transcripts (1 transcript mapped to 1 gene)
+valid_transcripts = get_valid_transcripts('../Genome_Files/unique_transcripts.txt')
+
+# get the allele counts for all sites with coverage
+chromo_sites = get_non_coding_snps('../SNP_files/', 10)
+print('got allele counts at all sites')
+
+# create a dict with coordinates of mature sequences
+miR_coord = {}
+infile = open('CRM_MatureCoordinatesFinal.txt')
 infile.readline()
 for line in infile:
     line = line.rstrip()
     if line != '':
         line = line.split('\t')
-        participant = line[1]
-        position = int(line[0]) - 1
-        if participant in Mutations:
-            Mutations[participant].add(position)
-        else:
-            Mutations[participant] = set()
-            Mutations[participant].add(position)
+        name, chromo, start, end, orientation = line[0], line[1], int(line[2]) -1, int(line[3]), line[4]
+        miR_coord[name] = [chromo, start, end, orientation]
 infile.close()
 
-# create a dict {participant: [# mutations, read_depth]}
-data = {}
-for participant in Mutations:
-    if participant in ReadDepth:
-        data[participant] = [len(Mutations[participant]), ReadDepth[participant]]
+# compute theta for each mirna
+# create dict to store the mirna: diversity pair
+diversity = {}
+# loop over mirna name in {name : [chromo, start, end, orientation, conservation]}
+for name in miR_coord:
+    chromo, start, end = miR_coord[name][0], miR_coord[name][1], miR_coord[name][2]
+    conservation = famCons[name]
+    theta = compute_theta_non_coding(chromo_sites, chromo, start, end, 2)
+    # check if theta is defined
+    if theta != 'NA':
+        diversity[name] = theta        
 
-# create 2 parallel lists
-mutations, reads = [], []
-for participant in data:
-    if data[participant][0] < 1000:
-        mutations.append(data[participant][0])
-        reads.append(data[participant][1])
+# remove mirnas from expression for which couldn't be computed
+to_remove = []
+for mirna in mature_expression:
+    if mirna not in diversity:
+        to_remove.append(mirna)
+print('remove {0} mirnas with expression but no diversity'.format(len(to_remove)))
+if len(to_remove) != 0:
+    for mirna in to_remove:
+        del mature_expression[mirna]
+# check that dicts have same keys
+assert mature_expression.keys() == diversity.keys(), 'expression and diversity should have same mirna keys'
 
-# make a scatter plot mutations X reads
+# create parallel lists of expression and diversity
+mature_theta = []
+expression_level = []
+for mirna in mature_expression:
+    expression_level.append(mature_expression[mirna])
+    mature_theta.append(diversity[mirna])
+print('made lists of expression and diversity values')
+
+
+# make a scatter plot diversity X expression
 
 # create figure
 fig = plt.figure(1, figsize = (4.3,2.56))
-
 # add a plot to figure (1 row, 1 column, 1 plot)
 ax = fig.add_subplot(1, 1, 1)    
 
 # write label for y axis
-ytext = ax.set_ylabel('Number of variable positions', color = 'black', size = 10, ha = 'center', fontname = 'Arial', family = 'sans-serif')
-xtext = ax.set_xlabel('{0} read depth for {1}'.format(read_depth.capitalize(), tumor), color = 'black', size = 10, ha = 'center', fontname = 'Arial', family = 'sans-serif')
+ytext = ax.set_ylabel('Mature miR polymorphism', color = 'black', size = 10, ha = 'center', fontname = 'Arial', family = 'sans-serif')
+if expression == 'male':
+    xtext = ax.set_xlabel('Mature miR expression level in males', color = 'black', size = 10, ha = 'center', fontname = 'Arial', family = 'sans-serif')
+elif expression == 'development':
+    xtext = ax.set_xlabel('Mature miR expression level across development', color = 'black', size = 10, ha = 'center', fontname = 'Arial', family = 'sans-serif')
 
 # add a light grey horizontal grid to the plot, semi-transparent, 
 ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)  
@@ -158,16 +137,17 @@ ax.set_axisbelow(True)
 ax.get_xaxis().tick_bottom()
 ax.get_yaxis().tick_left()
 
-ax.scatter(reads, mutations, edgecolor = 'red', facecolor = 'r', alpha = 0.8)
+# add data to plot
+ax.scatter(expression_level, mature_theta, edgecolor = 'red', facecolor = 'r', alpha = 0.8)
 
 # calc the trendline
-z = np.polyfit(reads, mutations, 1)
+z = np.polyfit(expression_level, mature_theta, 1)
 p = np.poly1d(z)
-ax.plot(reads,p(reads), linewidth = 1, color = 'black', linestyle = '-', alpha = 0.5)
+ax.plot(expression_level,p(expression_level), linewidth = 1, color = 'black', linestyle = '-', alpha = 0.5)
 
 
 # compute Spearman's rank correlation
-rho, pval = stats.spearmanr(mutations, reads)
+rho, pval = stats.spearmanr(expression_level, mature_theta)
 if pval > 0.05:
     P = 'NS'
 elif pval < 0.05 and pval > 0.01:
@@ -183,8 +163,8 @@ else:
 
 
 # annotate text
-y_min, y_max = min(mutations), max(mutations) 
-x_min, x_max = min(reads), max(reads)
+y_min, y_max = min(mature_theta), max(mature_theta) 
+x_min, x_max = min(expression_level), max(expression_level)
 
 ax.text(x_max, y_max, correlation, horizontalalignment='right',
         verticalalignment='center', fontsize = 8, fontname = 'Arial')
@@ -234,27 +214,10 @@ for label in ax.get_yticklabels():
     label.set_fontname('Arial')
     
 # add title
-plt.title('Heteroplasmies as a function of the {0} read depth\n'.format(read_depth), size = 12, fontname = 'Arial')  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#plt.title('Heteroplasmies as a function of the {0} read depth\n'.format(read_depth), size = 12, fontname = 'Arial')  
 
 # save figure
-fig.savefig(outputfile, bbox_inches = 'tight')
+fig.savefig('testfile.pdf', bbox_inches = 'tight')
     
     
  
