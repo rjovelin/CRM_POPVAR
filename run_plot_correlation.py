ba435d0097353fdafeaaf22b755fada0869e7d26
Richard <richard.jovelin@oicr.on.ca>
2016-05-20 16:41:49 -0400

    plot bar graph for miR expression and diversity

--- run_plot_correlation.py
+++ run_plot_correlation.py
@@ -5,11 +5,7 @@ Created on Thu May 19 16:28:06 2016
 @author: RJovelin
 """
 
-# use this script to plot the correlation between nucleotide diversity in mature sequences
-# and expression level of mature sequences
-
-
-
+# use this script to plot mature miR polymorphism as a function of mature expression level
 
 
 # use Agg backend on server without X server
@@ -27,11 +23,8 @@ from divergence import *
 from miRNA_target import *
 from sites_with_coverage import *
 
-
-
-# get option to use expression in males or across development
-expression = sys.argv[1]
-
+# expression is the average expression level across different time points
+# including larval and embryonic development
 
 # create a dict to store the expression level of mature miRNAs
 mature_expression = {}
@@ -45,25 +38,13 @@ for line in infile:
         mirna = line[0]
         # check that mirna has expression
         if len(line) >= 17:
-            # get expression
-            if expression == 'male':
-                # take expression in remanei males
-                level = float(line[17])
-            elif expression == 'development':
-                # take mean of expression at different time points
-                level = np.mean(list(map(lambda x: float(x), line[9:17])))
+            # take mean of expression at different time points
+            level = np.mean(list(map(lambda x: float(x), line[9:17])))
             # populate dict
             mature_expression[mirna] = level
         else:
             print('no expression for {0}'.format(mirna))
 print('recorded miR expression')
-
-
-############################
-
-
-# create a set of valid transcripts (1 transcript mapped to 1 gene)
-valid_transcripts = get_valid_transcripts('../Genome_Files/unique_transcripts.txt')
 
 # get the allele counts for all sites with coverage
 chromo_sites = get_non_coding_snps('../SNP_files/', 10)
@@ -113,119 +94,142 @@ if len(to_remove) != 0:
 # check that dicts have same keys
 assert mature_expression.keys() == diversity.keys(), 'expression and diversity should have same mirna keys'
 
-# create parallel lists of expression and diversity
-mature_theta = []
-expression_level = []
-for mirna in mature_expression:
-    expression_level.append(mature_expression[mirna])
-    mature_theta.append(diversity[mirna])
-print('made lists of expression and diversity values')
+# create list of expression values
+expression_level = [mature_expression[mirna] for mirna in mature_expression]
+print('made list of expression values')
 
+# compute quartiles of expression values
+Q1 = np.percentile(expression_level, 25)
+Q2 = np.percentile(expression_level, 50)
+Q3 = np.percentile(expression_level, 75)
+print('computed quartiles of expression distribution')
+print(Q1, Q2, Q3)
 
-# make a scatter plot diversity X expression
+# partition thetas according to expression quartiles
+# create lists to store theta for different levels of expression
+lowExp, moderateExp, mediumExp, highExp = [], [], [], []
+# loop over mirna in theta dict
+for mirna in diversity:
+    # check expression value
+    if mature_expression[mirna] < Q1:
+        lowExp.append(diversity[mirna])
+    elif mature_expression[mirna] >= Q1 and mature_expression[mirna] < Q2:
+        moderateExp.append(diversity[mirna])
+    elif mature_expression[mirna] >= Q2 and mature_expression[mirna] < Q3:
+        mediumExp.append(diversity[mirna])
+    elif mature_expression[mirna] >= Q3:
+        highExp.append(diversity[mirna])
+
+# verify that mirnas belong to all levels of expression
+assert len(lowExp) != 0
+assert len(moderateExp) != 0
+assert len(mediumExp) != 0
+assert len(highExp) != 0
+print('low', len(lowExp))
+print('moderate', len(moderateExp))
+print('medium', len(mediumExp))
+print('high', len(highExp))
+
+# create list of data
+alldata = [lowExp, moderateExp, mediumExp, highExp]
+# create a list of expression levels
+ExpLevels = ['Low', 'Moderate', 'Medium', 'High']
 
 # create figure
-fig = plt.figure(1, figsize = (4.3,2.56))
-# add a plot to figure (1 row, 1 column, 1 plot)
-ax = fig.add_subplot(1, 1, 1)    
+fig = plt.figure(1, figsize = (2,2))
+# add a plot to figure (1 row, 1 column, 1st plot)
+ax = fig.add_subplot(1, 1, 1)
 
-# write label for y axis
-ytext = ax.set_ylabel('Mature miR polymorphism', color = 'black', size = 10, ha = 'center', fontname = 'Arial', family = 'sans-serif')
-if expression == 'male':
-    xtext = ax.set_xlabel('Mature miR expression level in males', color = 'black', size = 10, ha = 'center', fontname = 'Arial', family = 'sans-serif')
-elif expression == 'development':
-    xtext = ax.set_xlabel('Mature miR expression level across development', color = 'black', size = 10, ha = 'center', fontname = 'Arial', family = 'sans-serif')
+width = 0.8
+ind = np.arange(len(alldata))
+Means = [np.mean(i) for i in alldata]
+SEM = []
+for i in alldata:
+    SEM.append(np.std(i) / math.sqrt(len(i)))
+# use a bar plot
+ax.bar(ind, Means, width, yerr = SEM,
+       color = ['#fef0d9','#fdcc8a','#fc8d59','#d7301f'],
+       linewidth = 1.5, error_kw=dict(elinewidth=1.5, ecolor='black', markeredgewidth = 1.5))               
+    
+# restrict the x and y axis to the range of data
+ax.set_ylim([0, 0.014])
 
-# add a light grey horizontal grid to the plot, semi-transparent, 
-ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)  
-# hide these grids behind plot objects
-ax.set_axisbelow(True)
+# remove lines around the frame
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.spines['bottom'].set_visible(True)
+# offset the spines
+for spine in ax.spines.values():
+    spine.set_position(('outward', 5))
 
-# remove top axes and right axes ticks
-ax.get_xaxis().tick_bottom()
-ax.get_yaxis().tick_left()
-
-# add data to plot
-ax.scatter(expression_level, mature_theta, edgecolor = '#f03b20', facecolor = '#f03b20', alpha = 0.8)
-
-# calc the trendline
-z = np.polyfit(expression_level, mature_theta, 1)
-p = np.poly1d(z)
-ax.plot(expression_level,p(expression_level), linewidth = 1, color = 'black', linestyle = '-', alpha = 0.5)
-
-
-# compute Spearman's rank correlation
-rho, pval = stats.spearmanr(expression_level, mature_theta)
-if pval > 0.05:
-    P = 'NS'
-elif pval < 0.05 and pval > 0.01:
-    P = '< 0.05'
-elif pval < 0.01 and pval > 0.001:
-    P = '< 0.01'
-elif pval < 0.001:
-    P = '< 0.001'
-if P == 'NS':
-    correlation = 'Spearman\'s r = {0}, P > 0.05'.format(round(rho, 3))
-else:
-    correlation = 'Spearman\'s r = {0}, P '.format(round(rho, 3)) + P
-
-
-# annotate text
-y_min, y_max = min(mature_theta), max(mature_theta) 
-x_min, x_max = min(expression_level), max(expression_level)
-
-ax.text(x_max, y_max, correlation, horizontalalignment='right',
-        verticalalignment='center', fontsize = 8, fontname = 'Arial')
-        
-# do not show lines around figure  
-ax.spines["top"].set_visible(False)    
-ax.spines["bottom"].set_visible(False)    
-ax.spines["right"].set_visible(False)    
-ax.spines["left"].set_visible(False)  
-
-
-## create a list with range of x-axis values
-#xvals = [i + 0.5 for i in range(len(names) + 1)]
-## Set a buffer around the edge of the x-axis
-#plt.xlim([min(xvals)- 0.5, max(xvals)+ 0.5])
+# set x tick positions
+xtickpos = [i + width/2 for i in range(len(alldata))]
+ax.set_xticks(xtickpos)
 
 # do not show ticks
 plt.tick_params(
-    axis='y',       # changes apply to the x-axis and y-axis (other option : x, y)
+    axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
     which='both',      # both major and minor ticks are affected
     bottom='off',      # ticks along the bottom edge are off
     top='off',         # ticks along the top edge are off
     right = 'off',
     left = 'off',          
-    labelbottom='off', # labels along the bottom edge are off 
+    labelbottom= 'on', # labels along the bottom edge are off 
     colors = 'black',
-    labelsize = 10)
-      
-
-# do not show ticks
-plt.tick_params(
-    axis='x',       # changes apply to the x-axis and y-axis (other option : x, y)
-    which='both',      # both major and minor ticks are affected
-    bottom='on',      # ticks along the bottom edge are off
-    top='off',         # ticks along the top edge are off
-    right = 'off',
-    left = 'off',          
-    labelbottom='on', # labels along the bottom edge are on 
-    colors = 'lightgrey',
-    labelcolor = 'black',
     labelsize = 10,
     direction = 'out')
 
+# set up xtick labels
+plt.xticks(xtickpos, ExpLevels, rotation = 20, ha = 'right', size = 10, fontname = 'Arial')
 
 # Set the tick labels font name
 for label in ax.get_yticklabels():
     label.set_fontname('Arial')
-    
-# add title
-#plt.title('Heteroplasmies as a function of the {0} read depth\n'.format(read_depth), size = 12, fontname = 'Arial')  
+
+# write label for x and y axis
+ax.set_ylabel('Nucleotide polymorphism', color = 'black',  size = 10, ha = 'center', fontname = 'Arial')
+ax.set_xlabel('miR expression level', color = 'black', size = 10, ha = 'center', fontname = 'Arial')
+
+# add margins
+ax.margins(0.05)
+
+# add a light grey horizontal grid to the plot, semi-transparent, 
+ax.yaxis.grid(True, linestyle='--', which='major', color='lightgrey', alpha=0.5)  
+# hide these grids behind plot objects
+ax.set_axisbelow(True)
+
+# compare mean differences among conservation levels
+for i in range(len(alldata) -1):
+    for j in range(i+1, len(alldata)):
+        # get the P value of Wilcoxon rank sum test
+        Pval = stats.ranksums(alldata[i], alldata[j])[1]
+        # get stars for significance
+        if Pval > 0.05:
+            P = 'N.S.'
+        elif Pval < 0.05 and Pval > 0.01:
+            P = '*'
+        elif Pval < 0.01 and Pval > 0.001:
+            P = '**'
+        elif Pval < 0.001:
+            P = '***'
+        print(ExpLevels[i], ExpLevels[j], Pval)    
+        
+# I already determined that all site categories are significantly different
+# using Wilcoxon rank sum tests, so we need now to add letters to show significance
+
+# annotate figure to add significance
+# get the x and y coordinates
+y_pos = [0.0142, 0.0123, 0.0045, 0.0025]
+x_pos = [i + width/2 for i in range(len(site_types))]
+diff = ['A', 'A,B', 'B,C', 'C']
+
+for i in range(len(diff)):
+    ax.text(x_pos[i], y_pos[i], diff[i], horizontalalignment='center',
+            verticalalignment='center', color = 'black', fontname = 'Arial', size = 10)
 
 # save figure
-fig.savefig('testfile_' + expression + '.pdf', bbox_inches = 'tight')
+fig.savefig('DiversityMatureExpression.pdf', bbox_inches = 'tight')
     
     
  
