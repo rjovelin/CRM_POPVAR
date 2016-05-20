1f556592e7cfca9176d9c4bd426aef765f1ed9a9
Richard <richard.jovelin@oicr.on.ca>
2016-05-20 19:30:19 -0400

    added description and changed file paths

--- run_find_transcripts_with_indels.py
+++ run_find_transcripts_with_indels.py
@@ -5,25 +5,29 @@ Created on Fri Jun 12 17:00:04 2015
 @author: Richard
 """
 
+# use this script to generate a set a transcripts with indels in coding sequences in KSR and PX strains
+# save all transcript names to a text file
+
+
 from indel_coordinates import *
 
 # find transcrtips with deletions in KSR
-KSR_del = identify_genes_with_indels('../CREM_CLA_protein_divergence/356_10172014.gff3', 'KSR_D.compiled_pindel.txt')
+KSR_del = identify_genes_with_indels('356_10172014.gff3', 'KSR_D.compiled_pindel.txt')
 print('identified deletions in KSR')
 print(len(KSR_del))
 
 # find transcripts with insertions in KSR
-KSR_ins = identify_genes_with_indels('../CREM_CLA_protein_divergence/356_10172014.gff3', 'KSR_SI.compiled_pindel.txt')
+KSR_ins = identify_genes_with_indels('356_10172014.gff3', 'KSR_SI.compiled_pindel.txt')
 print('identified insertions in KSR')
 print(len(KSR_ins))
 
 # find transcripts with deletions in PX
-PX_del = identify_genes_with_indels('../CREM_CLA_protein_divergence/356_10172014.gff3', 'PX_D.compiled_pindel.txt')
+PX_del = identify_genes_with_indels('356_10172014.gff3', 'PX_D.compiled_pindel.txt')
 print('identified deletions in PX')
 print(len(PX_del))
 
 # find transcripts with insertions in PX
-PX_ins = identify_genes_with_indels('../CREM_CLA_protein_divergence/356_10172014.gff3', 'PX_SI.compiled_pindel.txt')
+PX_ins = identify_genes_with_indels('356_10172014.gff3', 'PX_SI.compiled_pindel.txt')
 print('identified insertions in PX')
 print(len(PX_ins))
 
