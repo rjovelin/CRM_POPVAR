cdead7724ad9ac04474fa1a72aa75b9dd9a4496f
Richard <richard.jovelin@oicr.on.ca>
2016-05-21 00:07:46 -0400

    added option to plot repeat or gene density

--- PlotChromoWideGeneRepeatDiversity.py
+++ PlotChromoWideGeneRepeatDiversity.py
@@ -5,28 +5,42 @@ Created on Thu Aug 20 13:17:35 2015
 @author: RJovelin
 """
 
-from accessories import *
-from piRNAs import *
+# use this script to plot nucleotide diversity with repeat or gene density for 
+# linkage groups LG1, LG2 and LG4
+
+# usage python3 PlotChromoWideGeneRepeatDiversity.py [options]
+#- [repeats/genes] : plot gene or repeat density along with nucleotide diversity
+
+
+# use Agg backend on server without X server
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+# import modules
+import numpy as np
+from scipy import stats
+import math
+import sys
+# import custom modules
+from manipulate_sequences import *
+#from piRNAs import *
 from miRNA_target import *
 from repeats_TEs import *
 from sliding_windows import *
 from sites_with_coverage import *
 from divergence import *
-import numpy as np
-from scipy import stats
-import math
 
 
 # convert genome fasta to dict
-genome = convert_fasta('noamb_356_v1_4.txt')
+genome = convert_fasta('../Genome_Files/noamb_356_v1_4.txt')
 print('genome converted to fasta dict')
 
 # get the gene coordinates {gene1 : [chromo, start, end, sense]}
-genes_coord = get_genes_coordinates('356_10172014.gff3')
+genes_coord = get_genes_coordinates('../Genome_Files/356_10172014.gff3')
 print('got gene coordinates')
 
 # get the set of valid transcripts
-valid_transcripts = get_valid_transcripts('unique_transcripts.txt')
+valid_transcripts = get_valid_transcripts('../Genome_Files/unique_transcripts.txt')
 print('got the set of valid transcripts')
 
 # get a dict with {chromo: [start position (0-based) of the valid transcripts]}
@@ -47,7 +61,7 @@ print('got the genes\'s first positions')
 
 # get the coordinates of the repeats
 # {repeat : [[chromo, start, end, orientation], [chromo, start, end, orientation]]}
-repeats_coord = get_repeats_coord('../piRNAs/356_v1_4.fasta.out', False)
+repeats_coord = get_repeats_coord('../Genome_Files/356_v1_4.fasta.out', False)
 print('got the repeat coordinates')
 
 # create a dict with {chromo: [list of repeat start positions 0-based]}
@@ -71,6 +85,76 @@ for fam in repeats_coord:
 for chromo in repeats_start:
     repeats_start[chromo].sort()
 print('got the repeats\'s first positions')
+
+
+
+
+########################## REMOVE function
+
+# use this function to generate save the number of pirnas per window
+def cluster_pirnas(pirna_start, genome, chromo, window_size, outputfile):
+    '''
+    (dict, dict, str, int, file) -> file
+    Take a dictionnary with chromo: list of start position pairs, a dict
+    with chromo : sequence pairs, the focal chromosome, the size of the window,
+    and save the number of pirnas per window in outputfile
+    '''
+    
+    # make list of size window that contains only 0s:
+    # each value in the list is the count of position for the range [0 - window[ etc
+    range_counts = [0] * (len(genome[chromo]) // window_size)
+    
+    # loop over starting positions
+    for start in pirna_start[chromo]:
+        # determine the index in the list range_count where the position should be added
+        which_range = start // window_size
+        if which_range == len(range_counts):
+            which_range -= 1
+        # count pirnas
+        range_counts[which_range] += 1
+        
+    # open file for writing
+    newfile = open(outputfile, 'w')
+    # write header
+    newfile.write('Range' + '\t' + 'Lower_point' + '\t' + 'Midpoint' + '\t' + 'Higher_point' + '\t' + 'Count' + '\t' + '\n')
+        
+    # loop over indices of list
+    for i in range(len(range_counts)):
+        newfile.write('[' + str(i * window_size) + '-' + str((i * window_size) + window_size -1) + ']' + '\t')
+        newfile.write(str(i * window_size) + '\t')
+        newfile.write(str(int(((i * window_size) + window_size) / 2)) + '\t')
+        newfile.write(str((i * window_size) + window_size) + '\t')
+        newfile.write(str(range_counts[i]) + '\n')
+        
+    newfile.close()
+
+
+
+
+
+
+
+
+
+
+
+
+################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # create files with gene densities per 50 Kb window 
 cluster_pirnas(genes_start, genome, 'linkage_group_1', 50000, 'LG1_gene_density_50Kb.txt')
