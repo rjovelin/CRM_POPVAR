0846f96c41915331b25d1219cf15e3f9f14158c1
Richard <richard.jovelin@oicr.on.ca>
2016-05-25 15:29:34 -0400

    compute proportions of SNPs in each MAF bin

--- PlotMAFmiRNAs.py
+++ PlotMAFmiRNAs.py
@@ -37,6 +37,7 @@ MAF_REP = MAF_SNP('../Genome_Files/CDS_SNP_DIVERG.txt', '../Genome_Files/unique_
                   '../Genome_Files/transcripts_indels_CDS.txt', 'REP', 10)
 print('REP', len(MAF_REP))
 print('MAF for replacement sites done')
+
 # make a list with MAF of synonymous sites, excluding sites with sample size < 10
 MAF_SYN = MAF_SNP('../Genome_Files/CDS_SNP_DIVERG.txt', '../Genome_Files/unique_transcripts.txt', 
                   '../Genome_Files/transcripts_indels_CDS.txt', 'SYN', 10)
@@ -57,10 +58,6 @@ print('got allele counts for miRNAs')
 MAF_mirna = MAF_non_coding(mirna_sites)
 print('miRNAs', len(MAF_mirna))
 print('MAF for miRNA sites done')
-
-## get all the miRNA positions in genome
-#mirna_pos = get_small_rna_sites(mirnas_coord)
-#print('got miRNA positions')
 
 # get SNPs flanking miRNAs within 500 bp of the miRNAs
 mirna_flanking_snps = get_small_rna_flanking_SNPs(chromo_sites, mirnas_coord, 500)
@@ -151,45 +148,89 @@ print('SNPs at non-target UTR sites: ', non_target_snps)
 UTR_resampled_MAF = SNP_MAF_randomization(UTR_snps, 10000, 1000)
 print('reseampled SNPs in UTRs')
 
-## get the proportions of SNPs in each MAF bin
-#UTR_MAF_proportions = get_MAF_distribution_from_replicates(UTR_resampled_MAF)
-#
-## get the SNP proportions for the observed SNPs
-#empirical_all_MAF = SNP_proportions_MAF_bin(MAF_all_targets)
-#empirical_crm_MAF = SNP_proportions_MAF_bin(MAF_crm_targets)
-#empirical_crmcla_MAF = SNP_proportions_MAF_bin(MAF_crmcla_targets)
-#empirical_crmclacel_MAF = SNP_proportions_MAF_bin(MAF_crmclacel_targets)
-#
-#print(len(empirical_all_MAF))
-#print(len(empirical_crm_MAF))
-#print(len(empirical_crmcla_MAF))
-#print(len(empirical_crmclacel_MAF))
-#
-## creat a list of keys, being the MAF lower bound in the dicts with MAF proportions
-#maf_limit = [i for i in empirical_all_MAF]
-## sort list
-#maf_limit.sort()
-#
-
-###############
+# express MAF frequencies in %
+for i in range(len(MAF_REP)):
+    MAF_REP[i] = MAF_REP[i] * 100
+for i in range(len(MAF_SYN)):
+    MAF_SYN[i] = MAF_SYN[i] * 100
+for i in range(len(MAF_mirna)):
+    MAF_mirna[i] = MAF_mirna[i] * 100
+for i in range(len(MAF_targets)):
+    MAF_targets[i] = MAF_targets[i] * 100    
+for i in mirna_resampled_MAF:
+    for j in range(len(mirna_resampled_MAF[i])):
+        mirna_resampled_MAF[i][j] = mirna_resampled_MAF[i][j] * 100
+for i in UTR_resampled_MAF:
+    for j in range(len(UTR_resampled_MAF[i])):
+        UTR_resampled_MAF[i][j] = UTR_resampled_MAF[i][j] * 100
+print('conversion to % frequencies done')
 
 
+# make histograms to get the counts in each MAF bins
+MAF_REP_hist = np.histogram(MAF_REP, range(0, 51, 10))
+MAF_SYN_hist = np.histogram(MAF_SYN, range(0, 51, 10))
+MAF_mirna_hist = np.histogram(MAF_mirna, range(0, 51, 10))
+MAF_targets_hist = np.histogram(MAF_targets, range(0, 51, 10))
+# create dicts to store counts in MAF bins {replicate_number : [[MAF counts], [MAF freq]]}
+MAF_mirna_resampling = {}
+for i in mirna_resampled_MAF:
+    MAF_mirna_resampling[i] = np.histogram(mirna_resampled_MAF[i], range(0, 51, 10))
+MAF_UTR_resampling = {}
+for i in UTR_resampled_MAF:
+    MAF_UTR_resampling[i] = np.histogram(UTR_resampled_MAF[i], range(0, 51, 10))
+print('got SNP counts in each MAF bin')
+
+# get the proportions of SNPs in each MAF bins
+REP_freq = [i / sum(MAF_REP_hist[0]) for i in MAF_REP_hist[0]]
+print(sum(MAF_REP_hist[0]))
+print(MAF_REP_hist[0])
+print(REP_freq)
+SYN_freq = [i / sum(MAF_SYN_hist[0]) for i in MAF_SYN_hist[0]]
+mirna_freq = [i / sum(MAF_mirna_hist[0]) for i in MAF_mirna_hist[0]]
+targets_freq = [i / sum(MAF_targets_hist[0]) for i in MAF_targets_hist[0]]
+# check length of lists of SNP proportions
+for i in [REP_freq, SYN_freq, mirna_freq, targets_freq]:
+    assert len(i) == 5, 'there should be 5 values in list of SNP proportions'
+mirna_resampling, targets_resampling = {}, {}
+# get the snps proprtions for each replicate
+for i in MAF_mirna_resampling:
+    mirna_resampling[i] = [j / sum(MAF_mirna_resampling[i][0]) for j in MAF_mirna_resampling[i][0]]
+for i in MAF_UTR_resampling:
+    targets_resampling[i] = [j / sum(MAF_UTR_resampling[i][0]) for j in MAF_UTR_resampling[i][0]]
+# get the mean and SEM SNP proportions for resampling
+# create dicts with MAF bin index as value and list of SNP proportion as value    
+mirna_prop, targets_prop = {}, {}
+for i in mirna_resampling:
+    # check length of list of SNP proportions
+    assert len(mirna_resampling[i]) == 5, 'Number of SNP proportions is different than 5'
+    for j in range(len(mirna_resampling[i])):
+        if j not in mirna_prop:
+            mirna_prop[j] = []
+        mirna_prop[j].append(mirna_resampling[i][j])
+for i in targets_resampling:
+    # check length of list of SNP proportions
+    assert len(targets_resampling[i]) == 5, 'Number of SNP proportions is different than 5'
+    for j in range(len(targets_resampling[i])):
+        if j not in targets_prop:
+            targets_prop[j] = []
+        targets_prop[j].append(targets_resampling[i][j])
+assert len(targets_prop) == len(mirna_prop), 'should have same size'
+assert len(targets_prop) == 5, 'should have size 5'
+# take mean and SEM for each MAF bin
+nums = [i for i in mirna_prop]
+nums.sort()
+mirna_samplefreq, mirna_sem, target_samplefreq, target_sem = [], [], [], []
+for i in nums:
+    mirna_samplefreq.append(np.mean(mirna_prop[i]))
+    target_samplefreq.append(np.mean(targets_prop[i]))
+    mirna_sem.append(np.std(mirna_prop[i]) / math.sqrt(len(mirna_prop[i])))
+    target_sem.append(np.std(targets_prop[i]) / math.sqrt(len(targets_prop[i])))
+print('got SNP proportions in each MAF bin')
 
 
 
-###################### CONTNUE HERE
 
-## obsolete code
-## get the proportions of SNPs in each MAF bin 
-#mirna_MAF_proportions = get_MAF_distribution_from_replicates(mirna_resampled_MAF)
-## get the SNP proportions for the observed SNPs
-#empirical_mirna_MAF = SNP_proportions_MAF_bin(MAF_mirna)
-## creat a list of keys, being the MAF lower bound in the dicts with MAF proportions
-#maf_limit = [i for i in empirical_mirna_MAF]
-## sort list
-#maf_limit.sort()
-#
-#
+
 #average = np.mean(mirna_MAF_proportions[i])
 #stdev = np.std(mirna_MAF_proportions[i])
 #observed = empirical_mirna_MAF[i]
@@ -210,55 +251,114 @@ print('reseampled SNPs in UTRs')
 #    P01 = 'NS'
 
 
-#####################
-
-# express MAF frequencies in %
-for i in range(len(MAF_REP)):
-    MAF_REP[i] = MAF_REP[i] * 100
-for i in range(len(MAF_SYN)):
-    MAF_SYN[i] = MAF_SYN[i] * 100
-for i in range(len(MAF_mirna)):
-    MAF_mirna[i] = MAF_mirna[i] * 100
-for i in range(len(MAF_targets)):
-    MAF_targets[i] = MAF_targets[i] * 100    
-for i in mirna_resampled_MAF:
-    for j in range(len(mirna_resampled_MAF[i])):
-        mirna_resampled_MAF[i][j] = mirna_resampled_MAF[i][j] * 100
-for i in UTR_resampled_MAF:
-    for j in range(len(UTR_resampled_MAF[i])):
-        UTR_resampled_MAF[i][j] = UTR_resampled_MAF[i][j] * 100
-print('conversion to % frequencies done')
-
-
-# make a list of maximum frequencies
-maxfreq = []
-for i in mirna_resampled_MAF:
-    maxfreq.append(max(mirna_resampled_MAF[i]))
-for i in UTR_resampled_MAF:
-    maxfreq.append(max(UTR_resampled_MAF[i]))
-for i in [MAF_REP, MAF_SYN, MAF_mirna, MAF_targets]:
-    maxfreq.append(max(i))
-print(max(maxfreq))
-assert max(maxfreq) <= 50, 'MAF should be lower than 50%'
-
-
-
-
-
 
 
 
 #
-## make histograms
-#MAF_REP_hist = np.histogram(MAF_REP, range(0, 51, 10))
-#MAF_SYN_hist = np.histogram(MAF_SYN, range(0, 51, 10))
-#MAF_mirna_hist = np.histogram(MAF_mirna, range(0, 51, 10))
-#MAF_intergenic_hist = np.histogram(MAF_intergenic, range(0, 51, 10))
-## create a dict to store counts in MAF bins {replicate_number : [MAF counts]}
-#MAF_resampling = {}
-#for i in mirna_resampled_MAF:
-#    MAF_resampling[i] = np.histogram(mirna_resampled_MAF[i], range(0, 51, ))
+## loop over names
+#for j in range(len(names)):
+#    newfile.write('\n')
+#    newfile.write('MAF for ' + names[j] + '\n')
+#    newfile.write('\t'.join(['MAF', 'mean_sample', 'margin', 'stdev_sample', 'l95', 'h95', 'observed', 'z-score', 'alpha_0.05', 'alpha_0.01', 'alpha_0.001']) + '\n')
+#    # loop over sorted keys
+#    for i in maf_limit:
+#        # compute mean
+#        average = np.mean(UTR_MAF_proportions[i])
+#        # get standard deviation
+#        stdev = np.std(UTR_MAF_proportions[i])
+#        # compute 95% CI
+#        stderror = stdev / math.sqrt(len(UTR_MAF_proportions[i]))
+#        # compute the margin error (critical value = 1.96 for 95% CI)
+#        margin = 1.96 * stderror
+#        lCI = average - margin
+#        hCI = average + margin
+#        observed = empirical[j][i]
+#        z_score = (observed - average) / stdev
+#        # critical values for 1-sample 2-tailed z-test: 0.05: +- 1.96, 0.01: +- 2.58, 0.001: +-3.27
+#        # Ho : obsvered == mean, H1: observed != mean
+#        if z_score < -1.96 or z_score > 1.96:
+#            P5 = '*'
+#        elif -1.96 <= z_score <= 1.96:
+#            P5 = 'NS'
+#        if z_score < -2.58 or z_score > 2.58:
+#            P1 = '*'
+#        elif -2.58 <= z_score <= 2.58:
+#            P1 = 'NS'
+#        if z_score < -3.27 or z_score > 3.27:
+#            P01 = '*'
+#        elif -3.27 <= z_score <= 3.27:
+#            P01 = 'NS'
+#        newfile.write('\t'.join([str(i), str(average), str(margin), str(stdev), str(lCI), str(hCI), str(observed), str(z_score), str(P5), str(P1), str(P01)]) + '\n')
+#    
+## close file after writing
+#newfile.close()
+
+
+
+
+## get the proportions of SNPs in each MAF bin
+#UTR_MAF_proportions = get_MAF_distribution_from_replicates(UTR_resampled_MAF)
 #
+## get the SNP proportions for the observed SNPs
+#empirical_all_MAF = SNP_proportions_MAF_bin(MAF_all_targets)
+#empirical_crm_MAF = SNP_proportions_MAF_bin(MAF_crm_targets)
+#empirical_crmcla_MAF = SNP_proportions_MAF_bin(MAF_crmcla_targets)
+#empirical_crmclacel_MAF = SNP_proportions_MAF_bin(MAF_crmclacel_targets)
+#
+#print(len(empirical_all_MAF))
+#print(len(empirical_crm_MAF))
+#print(len(empirical_crmcla_MAF))
+#print(len(empirical_crmclacel_MAF))
+#
+## creat a list of keys, being the MAF lower bound in the dicts with MAF proportions
+#maf_limit = [i for i in empirical_all_MAF]
+## sort list
+#maf_limit.sort()
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #
 #print('histograms done')
 #
