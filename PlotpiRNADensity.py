0711569264986a2901c3438b5fdec224ad198f29
Richard <richard.jovelin@oicr.on.ca>
2016-05-26 17:51:43 -0400

    removed LG option; fixed x axis; changed line width

--- PlotpiRNADensity.py
+++ PlotpiRNADensity.py
@@ -5,11 +5,7 @@ Created on Thu May 26 15:24:34 2016
 @author: RJovelin
 """
 
-# use this script to plot piRNA density for linkage groups LG3, LG4 or LG13
-
-# usage python3 PlotpiRNADensity.py [options]
-#- [LG3/LG4/LG13] : chromo to plot
-
+# use this script to plot piRNA density for linkage groups LG4
 
 # use Agg backend on server without X server
 import matplotlib as mpl
@@ -27,26 +23,22 @@ from manipulate_sequences import *
 from piRNAs import *
 
 
-# get chromo from command
-LG = sys.argv[1]
-if LG == 'LG3':
-    chromo = 'linkage_group_3'
-elif LG == 'LG4':
-    chromo = 'linkage_group_4'
-elif LG == 'LG13':
-    chromo = 'linkage_group_13'
-
+# look for clusters on linkage group 4
+chromo = 'linkage_group_4'
 
 # get the start positions of piRNAs
 pirna_start = chromo_pirna_start('PX356_piRNA_coord.txt')
 # sort positions
-for chromo in pirna_start:
-    pirna_start[chromo].sort()
+for i in pirna_start:
+    pirna_start[i].sort()
+
 print('sorted the pirna start positions')
     
 # convert genome fasta to dict
 genome = convert_fasta('../Genome_Files/noamb_356_v1_4.txt')
 print('genome converted to fasta dict')
+print(chromo, len(genome[chromo]))
+print((len(genome[chromo]) // 100000))
 
 # create list with count of gene o repeat in 100000 bp windows
 range_counts = [0] * (len(genome[chromo]) // 100000)
@@ -56,12 +48,12 @@ for start in pirna_start[chromo]:
         which_range -= 1
     # count repeats
     range_counts[which_range] += 1
+    
 
 # create a list with the position of each window interval
 positions = [i for i in range(len(range_counts))]      
 print('position', len(positions))
-print(LG, len(genome[chromo]))
-print('interval length', len(genome[chrom]) // 100000)
+print('interval length', len(genome[chromo]) // 100000)
 
 # create figure
 fig = plt.figure(1, figsize = (4, 1))
@@ -69,18 +61,10 @@ fig = plt.figure(1, figsize = (4, 1))
 ax = fig.add_subplot(1, 1, 1)  
 
 # plot the repeat of gene density per window
-ax.plot(positions, range_counts, linewidth = 1, color = 'black')
+ax.plot(positions, range_counts, linewidth = 1.2, color = 'black')
 
-ax.set_ylabel('Number of piRNA /100 Kb', size = 10, ha = 'center', fontname = 'Arial')
+ax.set_ylabel('# piRNAs /100 Kb', size = 10, ha = 'center', fontname = 'Arial')
  
-# determine tick position on x axis
-xpos =  [j for j in range(0, len(positions) + 50, 50)]
-# convert interval windows numbers to genomic positions
-xtext = list(map(lambda x : (x * 50000) / 1000000, xpos))
-xtext = list(map(lambda x : str(x), xtext))
-# set up tick positions and labels
-plt.xticks(xpos, xtext, fontsize = 10, fontname = 'Arial')
-
 # set x axis label
 ax.set_xlabel('Position along linkage group (Mb)', size = 10, ha = 'center', fontname = 'Arial')
 
@@ -124,4 +108,12 @@ ax.tick_params(
     labelsize = 10,
     direction = 'out') # ticks are outside the frame when bottom = 'on
 
-fig.savefig('piRNADensity' + LG + '.pdf', bbox_inches = 'tight')
+# determine tick position on x axis
+xpos =  [j for j in range(0, len(positions), 10)]
+# convert interval windows numbers to genomic positions
+xtext = list(map(lambda x : (x * 100000) / 1000000, xpos))
+xtext = list(map(lambda x : str(int(x)), xtext))
+# set up tick positions and labels
+plt.xticks(xpos, xtext, fontsize = 10, fontname = 'Arial')
+
+fig.savefig('piRNADensityLG4.pdf', bbox_inches = 'tight')
