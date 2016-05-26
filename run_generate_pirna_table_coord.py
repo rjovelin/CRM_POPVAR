3f58941b6b1b7e457d6e238e48aa6dea4cc66863
Richard <richard.jovelin@oicr.on.ca>
2016-05-26 14:57:39 -0400

    changed file path and added description

--- run_generate_pirna_table_coord.py
+++ run_generate_pirna_table_coord.py
@@ -5,9 +5,14 @@ Created on Sat Jul 11 09:10:53 2015
 @author: Richard
 """
 
+# use this script to generate a file with the 
+
+
 from piRNAs import *
-from accessories import *
+from manipulate_sequences import *
 
-pirna_coord = map_remanei_pirnas('../CREM_CLA_protein_divergence/noamb_356_v1_4.txt', 'remanei_piRNA_upstream70.fa')
+# map the piRNAs and their upstream regions to the remanei genome 
+pirna_coord = map_remanei_pirnas('../Genome_Files/noamb_356_v1_4.txt', 'remanei_piRNA_upstream70.fa')
 
+# write piRNAs coordinates to file
 write_pirna_coordinates_to_file(pirna_coord, 'PX356_piRNA_coord.txt')
