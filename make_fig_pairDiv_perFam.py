567bd56d658b1fdab7b2fffef6c0f5452a326ea5
Richard <richard.jovelin@oicr.on.ca>
2016-06-02 19:53:33 -0400

    created plotting function

--- make_fig_pairDiv_perFam.py
+++ make_fig_pairDiv_perFam.py
@@ -30,11 +30,6 @@ from divergence import *
 folders = [folder for folder in os.listdir('./Pairwise_Chemos/') if '_family' in folder]
 # sort folders
 folders.sort()
-print('generated a list of directories')
-
-# multiply by 100 to get %
-for i in range(len(FamDist)):
-    FamDist[i] = FamDist[i] * 100
 
 # create a dictionnary 
 Distances = {}
@@ -43,12 +38,11 @@ Distances = {}
 for folder in folders:
     # create a list of filenames of aligned sequences
     files = [filename for filename in os.listdir('./Pairwise_Chemos/' + folder) if '.txt' in filename]
-    print(folder, len(files))    
     # create a list to store the distances
     distances = []
     # loop over files, convert to fasta
     for filename in files:
-        proteins = convert_fasta('./Pairwise_Chemos/' + folder + './' + filename)
+        proteins = convert_fasta('./Pairwise_Chemos/' + folder + '/' + filename)
         # get the sequences
         genes = [i for i in proteins]
         seq1 = proteins[genes[0]]
@@ -64,109 +58,113 @@ for folder in folders:
     # grab family name
     family = folder[:folder.index('_family')]
     # populate dict with pairs counts
-    Distance[family] = hist[0]
-
-
-# print pairs counts    
-print('Distances', len(Distances))
-for family in Distances:
-    print(family, sum(Distances[family]))
-    
+    Distances[family] = list(hist[0])
 
 # create figure
-fig = plt.figure(1, figsize = (6,2.5))
-# add a plot to figure (1 row, 1 column, 1 plot)
-ax = fig.add_subplot(1, 1, 1)  
+fig = plt.figure(1, figsize = (6, 6))
+## adjust white space between subplots
+#fig.subplots_adjust(left=0.2, wspace=0.6)
 
 # make a list of families
 families = [i for i in Distances]
+families.sort()
 
 
-#############################
+# create a function to format the subplots
+def CreateAx(Columns, Rows, Position, Xscale, Data, figure, Title, width, XLabel = False, YLabel = False, BottomLine = False):
+    # create subplot in figure
+    # add a plot to figure (N row, N column, plot N)
+    ax = figure.add_subplot(Rows, Columns, Position)
+    ax.bar(Xscale, Data, width, color = 'black', edgecolor = 'black', linewidth = 1)
+    ax.set_title(Title, size = 10)
+    
+    if YLabel == True:
+        # set y axis label
+        ax.set_ylabel('Protein pairs', size = 7, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
+    if XLabel == True:
+        # set x axis label
+        ax.set_xlabel('Protein distance', size = 7, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
 
+    # do not show lines around figure, keep bottow line  
+    ax.spines["top"].set_visible(False)    
+    ax.spines["right"].set_visible(False)    
+    ax.spines["left"].set_visible(False)      
+    
+    if BottomLine == True:
+        ax.spines["bottom"].set_visible(True)
+    else:
+        ax.spines["bottom"].set_visible(False)
+    
+        # add a light grey horizontal grid to the plot, semi-transparent, 
+    ax.yaxis.grid(True, linestyle=':', which='major', color='lightgrey', alpha=0.5, linewidth = 0.5)  
+    # hide these grids behind plot objects
+    ax.set_axisbelow(True)    
+        
+    if BottomLine == True:
+        # do not show ticks
+        plt.tick_params(
+            axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
+            which='both',      # both major and minor ticks are affected
+            bottom='on',      # ticks along the bottom edge are off
+            top='off',         # ticks along the top edge are off
+            right = 'off',
+            left = 'off',          
+            labelbottom='on', # labels along the bottom edge are on
+            colors = 'black',
+            labelsize = 7,
+            direction = 'out') # ticks are outside the frame when bottom = 'on'  
+    else:
+        # do not show ticks
+        plt.tick_params(
+            axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
+            which='both',      # both major and minor ticks are affected
+            bottom='off',      # ticks along the bottom edge are off
+            top='off',         # ticks along the top edge are off
+            right = 'off',
+            left = 'off',          
+            labelbottom='off', # labels along the bottom edge are on
+            colors = 'black',
+            labelsize = 7,
+            direction = 'out') # ticks are outside the frame when bottom = 'on'  
 
-# set width of bar
+    if XLabel == True:
+        # determine tick position on x axis
+        xpos =  [i /10  for i in range(11)]
+        Dist = ['0', '', '', '', '', '0.5', '', '', '', '', '1']
+        # set up tick positions and labels
+        plt.xticks(xpos, Dist, rotation = 0, fontsize = 7, ha = 'center', fontname = 'Helvetica')
+
+    if YLabel == True:
+        plt.yticks(fontsize = 8)
+  
+    return ax
+
+# set width for all subplots    
 width = 0.1
 
+ax1 = CreateAx(4, 5, 1, [i / 10 for i in range(10)], Distances[families[0]], fig, families[0], width, YLabel = True)
+ax2 = CreateAx(4, 5, 2, [i / 10 for i in range(10)], Distances[families[1]], fig, families[1], width)
+ax3 = CreateAx(4, 5, 3, [i / 10 for i in range(10)], Distances[families[2]], fig, families[2], width)
+ax4 = CreateAx(4, 5, 4, [i / 10 for i in range(10)], Distances[families[3]], fig, families[3], width)
+ax5 = CreateAx(4, 5, 5, [i / 10 for i in range(10)], Distances[families[4]], fig, families[4], width, YLabel = True)
+ax6 = CreateAx(4, 5, 6, [i / 10 for i in range(10)], Distances[families[5]], fig, families[5], width)
+ax7 = CreateAx(4, 5, 7, [i / 10 for i in range(10)], Distances[families[6]], fig, families[6], width)
+ax8 = CreateAx(4, 5, 8, [i / 10 for i in range(10)], Distances[families[7]], fig, families[7], width)
+ax9 = CreateAx(4, 5, 9, [i / 10 for i in range(10)], Distances[families[8]], fig, families[8], width, YLabel = True)
+ax10 = CreateAx(4, 5, 10, [i / 10 for i in range(10)], Distances[families[9]], fig, families[9], width)
+ax11 = CreateAx(4, 5, 11, [i / 10 for i in range(10)], Distances[families[10]], fig, families[10], width)
+ax12 = CreateAx(4, 5, 12, [i / 10 for i in range(10)], Distances[families[11]], fig, families[11], width)
+ax13 = CreateAx(4, 5, 13, [i / 10 for i in range(10)], Distances[families[12]], fig, families[12], width, YLabel = True)
+ax14 = CreateAx(4, 5, 14, [i / 10 for i in range(10)], Distances[families[13]], fig, families[13], width)
+ax15 = CreateAx(4, 5, 15, [i / 10 for i in range(10)], Distances[families[14]], fig, families[14], width)
+ax16 = CreateAx(4, 5, 16, [i / 10 for i in range(10)], Distances[families[15]], fig, families[15], width)
+ax17 = CreateAx(4, 5, 17, [i / 10 for i in range(10)], Distances[families[16]], fig, families[16], width, XLabel = True, YLabel = True, BottomLine = True)
+ax18 = CreateAx(4, 5, 18, [i / 10 for i in range(10)], Distances[families[17]], fig, families[17], width, XLabel = True, BottomLine = True)
+ax19 = CreateAx(4, 5, 19, [i / 10 for i in range(10)], Distances[families[18]], fig, families[18], width, XLabel = True, BottomLine = True)
 
-# plot SNP proportions SYN
-graph1 = ax.bar([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], Distances[families[0]], width, color = 'black', edgecolor = 'black', linewidth = 1)
-# plot SNP proportions REP
-graph2 = ax.bar([1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2], Distances[families[1]], width, color = 'black', edgecolor = 'black', linewidth = 1)
-# plot SNP proportions miRNAs
-graph3 = ax.bar([2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3, 3.1], Distances[families[2]], width, color = 'black', edgecolor = 'black', linewidth = 1)
-
-
-
-
-
-
-
-
-
-################################
-
-# set y axis label
-ax.set_ylabel('Number of protein pairs', size = 10, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
-
-# set x axis label
-ax.set_xlabel('Protein distance', size = 10, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
-
-## remove top axes and right axes ticks
-#ax.get_xaxis().tick_bottom()
-#ax.get_yaxis().tick_left()
-
-# do not show lines around figure, keep bottow line  
-ax.spines["top"].set_visible(False)    
-ax.spines["bottom"].set_visible(True)    
-ax.spines["right"].set_visible(False)    
-ax.spines["left"].set_visible(False)      
-# offset the spines
-for spine in ax.spines.values():
-  spine.set_position(('outward', 5))  
-  
-# add a light grey horizontal grid to the plot, semi-transparent, 
-ax.yaxis.grid(True, linestyle='--', which='major', color='lightgrey', alpha=0.5)  
-# hide these grids behind plot objects
-ax.set_axisbelow(True)
-  
-# do not show ticks
-plt.tick_params(
-    axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
-    which='both',      # both major and minor ticks are affected
-    bottom='on',      # ticks along the bottom edge are off
-    top='off',         # ticks along the top edge are off
-    right = 'off',
-    left = 'off',          
-    labelbottom='on', # labels along the bottom edge are on
-    colors = 'black',
-    labelsize = 10,
-    direction = 'out') # ticks are outside the frame when bottom = 'on'  
-
-
-
-# determine tick position on x axis
-xpos =  [i for i in range(0, 110, 10)]
-Dist = ['0', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1']
-# set up tick positions and labels
-plt.xticks(xpos, Dist, rotation = 0, fontsize = 10, ha = 'center', fontname = 'Helvetica')
-
-## add labels to x-ticks, rotate and align right
-#ax.set_xticklabels(range(0, 110, 10), rotation = 0, ha = 'center', size = 10, fontname = 'Helvetica', family = 'sans-serif')
-#
-#plt.yticks(fontsize = 10)
-#plt.xticks(range(0, 110, 10))
-
-# add margins on right and left of gragh
-plt.margins(0.05)
-
+# make sure subplots do not overlap
+plt.tight_layout()
 
 # save figure
-fig.savefig('testfile.pdf', bbox_inches = 'tight')
+fig.savefig('ChemoProtDivergenceFamilies.pdf', bbox_inches = 'tight')
 
-
-
-    
-    
-    
-    
