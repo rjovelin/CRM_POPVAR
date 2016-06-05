916011449d22e0d7ff384b0cdf8d44757e8e2ac9
Richard <richard.jovelin@oicr.on.ca>
2016-06-05 10:49:43 -0400

    compute Mk tests for all genes

--- mk_tests_chemogenes.py
+++ mk_tests_chemogenes.py
@@ -6,7 +6,9 @@ Created on Sat Jun  4 19:09:25 2016
 """
 
 
-# use this script to 
+# use this script to generate table with number of genes departing from neutrality with MK test
+# and alpha, the proportion of amino acid fixed by adaptive evolution estimated by the method of 
+# Smith and Eyre-Walker Science 2002
 
 
 # use Agg backend on server without X server
@@ -58,15 +60,21 @@ GPCRs = set(gene for gene in chemo if gene in transcripts)
 print('GPCRS', len(GPCRs))
 print('made set of valid chemo genes')
 
+# create a set of non-chemo gene
 NonGPCRs = set(gene for gene in transcripts if gene not in chemo)
 print('NonGPCRs', len(NonGPCRs))
 print('made set of valid non-chemo genes')
-
 
 # count divergence and polymorphisms in Ontario strains
 # eliminate singleton polymorphisms
 # dict in form {gene : [PN, PS, DN, DS]}        
 MK = count_polym_diverg('../Genome_Files/CDS_SNP_DIVERG.txt', 'KSR_PX', 'count', 0, 1)
+
+# remove genes that are not valid transcripts
+to_remove = [gene for gene in MK if gene not in transcripts]
+for gene in to_remove:
+    del MK[gene]
+print('removed {0} genes from MK analysis'.format(len(to_remove)))
 
 # perform MK test using Fisher exact test
 mk = MK_test(MK, 'fisher')
@@ -75,39 +83,48 @@ mk = MK_test(MK, 'fisher')
 significant = [gene for gene in mk if mk[gene][-1] < 0.05]
 nonsignificant = [gene for gene in mk if mk[gene][-1] >= 0.05]
 
-# determine genes with significant MK that are under positive or negative selection
-negative, positive = [], []
-for gene in MK:
-    # check if gene has significant departure from neutrality
-    if gene in significant:
-        # check if PS is defined
-        if MK[gene][1] != 0:
-            # compute ratio PN / PS
-            polymorphism = MK[gene][0] / MK[gene][1]
-        elif MK[gene][1] == 0:
-            # add small number to PS
-            polymorphism = MK[gene][0] / (MK[gene][1] + 0.1)
-        # check if DS is defined
-        if MK[gene][3] != 0:
-            # compute DN / DS
-            divergence = MK[gene][2] / MK[gene][3]
-        elif MK[gene][3] == 0:
-            # add small number to DS
-            divergence = MK[gene][2] / (MK[gene][3] + 0.1)
-        # check if divergence is lower r higher than polymorphism
-        if divergence > polymorphism:
-            # positive selection
-            positive.append(gene)
-        elif divergence < polymorphism:
-            negative.append(gene)
+# create a function to determine genes under positive or negative selection
+def NaturalSelection(SynDivCounts, Significant):
+    '''
+    (dict, list) -> (list, list)
+    Take the dictionary with polymorphism and divergence counts and a list of
+    genes with significant MK test and return a tuple with lists of genes
+    that are respectively under negative and positive selection
+    '''
+    negative, positive = [], []
+    for gene in SynDivCounts:
+        # check if gene has significant departure from neutrality
+        if gene in Significant:
+            # check if PS is defined
+            if SynDivCounts[gene][1] != 0:
+                # compute ratio PN / PS
+                polymorphism = SynDivCounts[gene][0] / SynDivCounts[gene][1]
+            elif SynDivCounts[gene][1] == 0:
+                # add small number to PS
+                polymorphism = SynDivCounts[gene][0] / (SynDivCounts[gene][1] + 0.1)
+            # check if DS is defined
+            if SynDivCounts[gene][3] != 0:
+                # compute DN / DS
+                divergence = SynDivCounts[gene][2] / SynDivCounts[gene][3]
+            elif SynDivCounts[gene][3] == 0:
+                # add small number to DS
+                divergence = SynDivCounts[gene][2] / (SynDivCounts[gene][3] + 0.1)
+            # check if divergence is lower r higher than polymorphism
+            if divergence > polymorphism:
+                # positive selection
+                positive.append(gene)
+            elif divergence < polymorphism:
+                negative.append(gene)
+    return negative, positive
 
+
+# determine genes with significant MK that are under positive or negative selection
+negative, positive = NaturalSelection(MK, significant)
 
 print('significant', len(significant))
 print('positive', len(positive))
 print('negative', len(negative))
-
 assert len(mk) == len(positive) + len(negative) + len(nonsignificant)
-
 
 # create lists of GPCR and non-GPCR genes
 PositiveChemo = [gene for gene in positive if gene in GPCRs]
@@ -124,6 +141,8 @@ print('chemo NS', len(NonsignificantChemo))
 print('NC +', len(PositiveNC))
 print('NC -', len(NegativeNC))
 print('NC NS', len(NonsignificantNC))
+assert len(mk) == len(PositiveChemo) + len(NegativeChemo) + len(NonsignificantChemo) + \
+                  len(PositiveNC) + len(NegativeNC) + len(NonsignificantNC)
 
 # apply a Bejamini-Hochberg correction for multiple testing {gene: corrected_Pval}
 mk_p = {}
@@ -135,98 +154,28 @@ corrected = Benjamini_Hochberg_correction(pvals)
 # count the number of genes with significant MK after BJ correction with 10% FDR 
 FDR = 0.1
 signifcorr = [gene for gene in corrected if corrected[gene] < FDR]
-print('FDR', FDR, len(signifcorr))
 nonsignifcorr = [gene for gene in corrected if corrected[gene] >= FDR]
+print('FDR', FDR, len(signifcorr))
+print('FDR', FDR, len(nonsignifcorr))
 
 # determine genes with significant MK that are under positive or negative selection
-negativecorr, positivecorr = [], []
-for gene in MK:
-    # check if gene has significant departure from neutrality
-    if gene in signifcorr:
-        # check if PS is defined
-        if MK[gene][1] != 0:
-            # compute ratio PN / PS
-            polymorphism = MK[gene][0] / MK[gene][1]
-        elif MK[gene][1] == 0:
-            # add small number to PS
-            polymorphism = MK[gene][0] / (MK[gene][1] + 0.1)
-        # check if DS is defined
-        if MK[gene][3] != 0:
-            # compute DN / DS
-            divergence = MK[gene][2] / MK[gene][3]
-        elif MK[gene][3] == 0:
-            # add small number to DS
-            divergence = MK[gene][2] / (MK[gene][3] + 0.1)
-        # check if divergence is lower r higher than polymorphism
-        if divergence > polymorphism:
-            # positive selection
-            positivecorr.append(gene)
-        elif divergence < polymorphism:
-            negativecorr.append(gene)
-
-
+negativecorr, positivecorr = NaturalSelection(MK, signifcorr)
 
 # create lists of GPCR and non-GPCR genes
-PositiveChemo = [gene for gene in positivecorr if gene in GPCRs]
-NegativeChemo = [gene for gene in negativecorr if gene in GPCRs]
-NonsignificantChemo = [gene for gene in nonsignifcorr if gene in GPCRs]
+PositiveChemoCorr = [gene for gene in positivecorr if gene in GPCRs]
+NegativeChemoCorr = [gene for gene in negativecorr if gene in GPCRs]
+NonsignificantChemoCorr = [gene for gene in nonsignifcorr if gene in GPCRs]
 
-PositiveNC = [gene for gene in positivecorr if gene in NonGPCRs]
-NegativeNC = [gene for gene in negativecorr if gene in NonGPCRs]
-NonsignificantNC = [gene for gene in nonsignifcorr if gene in NonGPCRs]
+PositiveNCCorr = [gene for gene in positivecorr if gene in NonGPCRs]
+NegativeNCCorr = [gene for gene in negativecorr if gene in NonGPCRs]
+NonsignificantNCCorr = [gene for gene in nonsignifcorr if gene in NonGPCRs]
 
-print('chemo +', len(PositiveChemo))
-print('chemo -', len(NegativeChemo))
-print('chemo NS', len(NonsignificantChemo))
-print('NC +', len(PositiveNC))
-print('NC -', len(NegativeNC))
-print('NC NS', len(NonsignificantNC))
+print('chemo +', len(PositiveChemoCorr))
+print('chemo -', len(NegativeChemoCorr))
+print('chemo NS', len(NonsignificantChemoCorr))
+print('NC +', len(PositiveNCCorr))
+print('NC -', len(NegativeNCCorr))
+print('NC NS', len(NonsignificantNCCorr))
+assert len(mk) == len(PositiveChemoCorr) + len(NegativeChemoCorr) + len(NonsignificantChemoCorr) + \
+                  len(PositiveNCCorr) + len(NegativeNCCorr) + len(NonsignificantNCCorr)
 
-for gene in PositiveChemo:
-    for family in Families:
-        if gene in Families[family]:
-            print(family)
-
-
-## determine the number of genes in expression groups for significant genes after correction
-## use a 10% FDR
-#FDR = 0.1
-#low_positive_corrected = [gene for gene in corrected if gene in low_positive and corrected[gene] < FDR]
-#midlow_positive_corrected = [gene for gene in corrected if gene in midlow_positive and corrected[gene] < FDR]
-#midhigh_positive_corrected = [gene for gene in corrected if gene in midhigh_positive and corrected[gene] < FDR]
-#high_positive_corrected = [gene for gene in corrected if gene in high_positive and corrected[gene] < FDR]
-#
-#low_negative_corrected = [gene for gene in corrected if gene in low_negative and corrected[gene] < FDR]
-#midlow_negative_corrected = [gene for gene in corrected if gene in midlow_negative and corrected[gene] < FDR]
-#midhigh_neggative_corrected = [gene for gene in corrected if gene in midhigh_negative and corrected[gene] < FDR]
-#high_negative_corrected = [gene for gene in corrected if gene in high_negative and corrected[gene] < FDR]
-#
-## get a list of genes with significant MK test after correction
-#significant_corrected = [gene for gene in corrected if corrected[gene] < FDR]
-## get a list of genes with positive selection after correction
-#positive_corrected = [gene for gene in corrected if gene in positive and corrected[gene] < FDR]
-## get a list of genes with negative selection after correction
-#negative_corrected = [gene for gene in corrected if gene in negative and corrected[gene] < FDR]
-#
-#
-#
-#
-#
-#
-#
-## create a dict of GO terms : genes
-#GO = GO_to_genes('../Chemoreceptors/PX356_protein_seq.tsv', 'unique_transcripts.txt')
-## test over=representation among genes with positive selection
-#GO_positive_pval = test_overepresentation(positive_corrected, GO)
-## test over-representation among genes with purifying selection
-#GO_negative_pval = test_overepresentation(negative_corrected, GO)
-#
-#
-#positive_over = [gene for gene in GO_positive_pval if GO_positive_pval[gene] < FDR]
-#negative_over = [gene for gene in GO_negative_pval if GO_negative_pval[gene] < FDR]
-#
-#print(len(positive_over))
-#print(len(negative_over))
-#
-#
-#
