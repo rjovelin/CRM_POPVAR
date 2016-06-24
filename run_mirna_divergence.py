2246c923b4ace07b3f1036eded8c0b2057c092a5
Richard <richard.jovelin@oicr.on.ca>
2016-06-24 16:18:41 -0400

    generate coordinate file and orthog file

--- run_mirna_divergence.py
+++ run_mirna_divergence.py
@@ -9,11 +9,10 @@ Created on Thu Jun 23 16:29:59 2016
 from manipulate_sequences import *
 from divergence import *
 
-# use this script to compute divergence between remanei and latens miRNAs
-
+# use this script to extract latens and remanei mirna orthologs
+# and to find the latens mirna coordinates
 
 # check that mutual blast hist retrieved the same best hists
-
 infile = open('../Genome_Files/ClaCrmCla_miRNABestBlast.txt')
 total = 0
 nonmatching = 0
@@ -31,11 +30,10 @@ for line in infile:
         total += 1
 infile.close()
 
-
 print('total', total)
 print('non-matching', nonmatching)
 print('proportion', nonmatching / total * 100)
-
+assert nonmatching == 0, 'some blast hists are not reciprocal'
 
 # remove any miRNAs with multiple hits
 
@@ -43,10 +41,7 @@ print('proportion', nonmatching / total * 100)
 ClaToCrm = {}
 # make a dict for crm to cla blast
 CrmToCla = {}
-
 infile = open('../Genome_Files/ClaCrmCla_miRNABestBlast.txt')
-total = 0
-nonmatching = 0
 for line in infile:
     if line.rstrip() != '':
         line = line.rstrip().split('\t')
@@ -60,17 +55,15 @@ for line in infile:
             CrmToCla[line[12]].append(line[13])
         else:
             CrmToCla[line[12]] = [line[13]]
-
+infile.close()
 # count cla and crm mirnas with more than 1 hist
 clamore, crmmore = 0, 0
 for mirna in ClaToCrm:
     if len(ClaToCrm[mirna]) > 1:
         clamore += 1
-        
 for mirna in CrmToCla:
     if len(CrmToCla[mirna]) > 1:
         crmmore += 1
-                
 print('crmmore', crmmore)
 print('clamore', clamore)
 
@@ -91,7 +84,6 @@ for mirna in CrmToCla:
         # also delete the latens mirnas
         for i in CrmToCla[mirna]:
             to_delete.add(i)
-        
 for mirna in to_delete:
     if mirna in ClaToCrm:
         del ClaToCrm[mirna]
@@ -99,18 +91,14 @@ for mirna in to_delete:
     if mirna in CrmToCla:
         del CrmToCla[mirna]
         removed_crm += 1
-
 print('removed from cla', removed_cla)
 print('removed from crm', removed_crm)
 print('crm', len(CrmToCla))
 print('cla', len(ClaToCrm))
 
-
 # make a dict of remanei : latens pairs
-
 # make sets of keys and vals for each dicts
 crmkeys, crmvals, clakeys, clavals = set(), set(), set(), set()
-
 for i in CrmToCla:
     crmkeys.add(i)
     assert len(CrmToCla[i]) == 1, 'crm should have a single cla match'
@@ -119,12 +107,78 @@ for i in ClaToCrm:
     clakeys.add(i)
     assert len(ClaToCrm[i]) == 1, 'cla should have a single crm match'
     clavals.add(ClaToCrm[i][0])
-
 # compare sets
 assert crmkeys == clavals, 'crm keys different than cla vals'
 assert clakeys == crmvals, 'cla keys than crm vals'
-
 # keys of crm to cla are vals of cla to crm, so can use either dict as final dict of remanei latens pairs
+
+# generate a file of remanei and latens mirna orthologs
+newfile = open('CremClamiRNAOrthologs.txt')
+for mir in CrmToCla:
+    # change names in lower caps
+    crmmirna = mir.lower()
+    clamirna = CrmToCla[mir][0].lower()
+    # change "new" to mir
+    if 'new' in crmmirna:
+        crmmirna = crmmirna.replace('new', 'mir')
+    if 'new' in clamirna:
+        clamirna = clamirna.replace('new', 'mir')
+    # replace mir -> miR
+    if 'mir' in crmmirna:
+        assert 'mir' in clamirna, "mir in crm name but not in cla name"
+        crmmirna = crmmirna.replace('mir', 'miR')
+        clamirna = clamirna.replace('mir', 'miR')
+    # replace "-5p" and "-3p" with "_5p" and "_3p" 
+    if '-5p' in crmmirna:
+        assert '-5p' in clamirna, '5p arm in crm does not match arm in cla ortholog'
+        crmmirna = crmmirna.replace('-5p', '_5p')
+        clamirna = clamirna.replace('-5p', '_5p')
+    elif '-3p' in crmmirna:
+        assert '-3p' in clamirna, '3p arm in crm does not match arm in cla ortholog' 
+        crmmirna = crmmirna.replace('-3p', '_3p')
+        clamirna = clamirna.replace('-3p', '_3p')
+    # write to file
+    newfile.write(crmmirna + '\t' + clamirna + '\n')
+newfile.close()
+
+
+# generate a file with latens coordinates 
+mirnas = {}
+infile = open('latens_coords.txt')
+infile.readline()
+for line in line:
+    if line.rstrip() != '':
+        line = line.rstrip().split('\t')
+        if len(line) ==8 and line[3] != '':
+            # get mirna name, hairpin, chromo, hairpin start
+            name, hairpin, chromo, start = line[0], line[1].upper(), line[2], int(line[3]) -1
+            # get mature, mature coord, and arm
+            mature, matcoord, arm = line[4].upper(), line[5], line[7]
+            # edit name
+            if 'new' in name:
+                name = name.replace('new', 'mir')
+            if 'mir' in name:
+                name = name.replace('mir', 'miR')
+            # add arm to name
+            name = name + '_' + arm
+            # parse mature coords
+            matstart = matcoord[matcoord.index(':')+1: matcoord.index('-')]
+            matstart = int(matstart) - 1
+            end = start + len(hairpin)
+            matend = matstart + len(mature)
+            # populate dict
+            mirnas[name] = [chromo, start, end, hairpin, matstart, matend, mature, arm]
+infile.close()
+
+newfile = open('Cla_miRNACoordinates.txt', 'w')
+#write header
+header = ['name', 'chromo', 'hairpin_start', 'hairpin_end', 'mature_start', 'mature_end', 'mature', 'arm']
+newfile.write('\t'.join(header) + '\n')
+
+
+
+
+
 
 
 # convert 
@@ -243,16 +297,6 @@ for mir in differences:
             strand = 'NA'
             divergent.append(mir)
 
-
-for mir in divergent:
-    print(mir)
-    print(sequences[mir][0])
-    print(sequences[mir][1])
-    print(sequences[mir][2])
-    print('*****')
-        
-
-
 print('nodiff', len(nodiff))
 print('close', len(close))
 print('divergent', len(divergent))
@@ -260,72 +304,68 @@ print('total', len(nodiff) + len(divergent) + len(close))
 
 
 
+try:
+   os.listdir('files_to_edit')
+except:
+    os.mkdir('files_to_edit')
 
-    # create lists of mirna names for divergent and related sequences 
-    divergent, close, nodiff = [], [], []
-
-    try:
-        os.listdir('files_to_edit')
-    except:
-        os.mkdir('files_to_edit')
-
-    # loop over differences 
-    for mir in differences:
-        # check if differences are large or low
-        # get mirna orientation
-        if differences[mir][0] <= 3:
-            strand = '+'
-            assert differences[mir][1] > 3, 'differences are not strand biased'
-            if differences[mir][0] > 0:
-                close.append(mir)
-            elif differences[mir][0] == 0:
-                nodiff.append(mir)
-        elif differences[mir][1] <= 3:
-            strand = '-'
-            assert differences[mir][0] > 3, 'differences are not strand biased'
-            if differences[mir][1] > 0:
-                close.append(mir)
-            elif differences[mir][1] == 0:
-                nodiff.append(mir)
-        elif differences[mir][0] > 3 and differences[mir][1] > 3:
-            strand = 'NA'
-            divergent.append(mir)
-        # add orientation to list
-        mirnas[mir].append(strand)
-        # get mature miR
-        mature = mirnas[mir][3]
-        # check if mirna needs manual curation
-        curation = False
-        if strand in '+-':
-            if strand == '+' and differences[mir][0] > 0:
-                # check if mature has differences
-                if mature not in sequences[mir][1]:
-                    curation = True
-                else:
-                    # no need for manual curation but replace hairpin with seq
-                    mirnas[mir][0] = sequences[mir][1]
-            elif strand == '-' and differences[mir][1] > 0:
-                # check if mature has differences
-                if mature not in sequences[mir][2]:
-                    curation = True
-                else:
-                    # no need for manual curation but replace hairpin with revseq
-                    mirnas[mir][0] = sequences[mir][2]
-        elif strand == 'NA':
-            curation = True
-        # check if manual curation is needed
-        if curation == True:
-            # add mature to file and do manual curation
-            newfile = open('files_to_edit/' + mir + '.fasta', 'w')
-            newfile.write('>crem\n')
-            newfile.write(sequences[mir][0] + '\n')
-            newfile.write('>seq\n')
-            newfile.write(sequences[mir][1] + '\n')
-            newfile.write('>revseq\n')
-            newfile.write(sequences[mir][2] + '\n')
-            newfile.write('>mature\n')
-            newfile.write(mature + '\n')
-            newfile.close()
+# loop over differences 
+for mir in differences:
+    # check if differences are large or low
+    # get mirna orientation
+    if differences[mir][0] <= 3:
+        strand = '+'
+        assert differences[mir][1] > 3, 'differences are not strand biased'
+        if differences[mir][0] > 0:
+            close.append(mir)
+        elif differences[mir][0] == 0:
+            nodiff.append(mir)
+    elif differences[mir][1] <= 3:
+        strand = '-'
+        assert differences[mir][0] > 3, 'differences are not strand biased'
+        if differences[mir][1] > 0:
+            close.append(mir)
+        elif differences[mir][1] == 0:
+            nodiff.append(mir)
+    elif differences[mir][0] > 3 and differences[mir][1] > 3:
+        strand = 'NA'
+        divergent.append(mir)
+    # add orientation to list
+    mirnas[mir].append(strand)
+    # get mature miR
+    mature = mirnas[mir][3]
+   # check if mirna needs manual curation
+    curation = False
+    if strand in '+-':
+        if strand == '+' and differences[mir][0] > 0:
+            # check if mature has differences
+            if mature not in sequences[mir][1]:
+                curation = True
+            else:
+                # no need for manual curation but replace hairpin with seq
+                mirnas[mir][0] = sequences[mir][1]
+        elif strand == '-' and differences[mir][1] > 0:
+            # check if mature has differences
+            if mature not in sequences[mir][2]:
+                curation = True
+            else:
+                # no need for manual curation but replace hairpin with revseq
+                mirnas[mir][0] = sequences[mir][2]
+    elif strand == 'NA':
+        curation = True
+    # check if manual curation is needed
+    if curation == True:
+        # add mature to file and do manual curation
+        newfile = open('files_to_edit/' + mir + '.fasta', 'w')
+        newfile.write('>crem\n')
+        newfile.write(sequences[mir][0] + '\n')
+        newfile.write('>seq\n')
+        newfile.write(sequences[mir][1] + '\n')
+        newfile.write('>revseq\n')
+        newfile.write(sequences[mir][2] + '\n')
+        newfile.write('>mature\n')
+        newfile.write(mature + '\n')
+        newfile.close()
 
     # check that all mirnas are dealt with
     assert len(mirnas) == len(nodiff) + len(close) + len(divergent), 'mirnas are missing'   
