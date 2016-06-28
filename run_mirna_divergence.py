6ff76abe59128c67351e886ddd4503757f2c180d
Richard <richard.jovelin@oicr.on.ca>
2016-06-28 16:36:35 -0400

    generate files with orthologous pairs

--- run_mirna_divergence.py
+++ run_mirna_divergence.py
@@ -7,19 +7,30 @@ Created on Tue Jun 28 13:50:20 2016
 
 # use this script to compute divergence between remanei and latens orthologs
 
-
+from tcoffee_alignment import *
 from manipulate_sequences import *
+import os
+
+
 
 
 # parse ortholog file to get the remanei and latens orthologs
 # create a dict of latens and remanei mirnas orthologs
-orthos = {}
+matureorthos = {}
 infile = open('CremClamiRNAOrthologs.txt')
 for line in infile:
     if line.rstrip() != '':
         line = line.rstrip().split('\t')
-        orthos[line[0]] = line[1]
+        matureorthos[line[0]] = line[1]
 infile.close()
+
+# create a dict with orthologs for hairpin sequences
+hairpinorthos = {}
+for mir in matureorthos:
+    # strip name from arm
+    crmname = mir[: mir.index('_')]
+    claname = matureorthos[mir][:matureorthos[mir].index('_')]
+    hairpinorthos[crmname] = claname
 
 # create a dict with chromo : sequence for remanei and for latens genomes
 ClaGenome = convert_fasta('../Genome_Files/noamb_534_v1.txt')
@@ -74,29 +85,79 @@ for line in infile:
         mirname = name[: name.index('_')]
         crmhairpin[mirname] = hairpin
 infile.close()
-        
+
+# create directory to save alignment files
+try:
+    os.listdir('Crm_Cla_miRNA_orthos')
+except:
+    os.mkdir('Crm_Cla_miRNA_orthos')
+
+# all pairs of hairpin sequences have been manually inspected in a first run    
+    
+# create files 
+for crmmirna in crmhairpin:
+    # if mirna has ortholog in latens
+    if crmmirna in hairpinorthos:
+        # get cla ortholog
+        clamirna = hairpinorthos[crmmirna]
+        if clamirna in clahairpin:
+            # open file for writing
+            # get file from mirna name
+            newfile = open('Crm_Cla_miRNA_orthos/' + crmmirna + '_hairpin.fasta', 'w')
+            newfile.write('>' + crmmirna + '\n')
+            newfile.write(crmhairpin[crmmirna] + '\n')
+            newfile.write('>' + clamirna + '\n')
+            if clamirna in ['cla-miR-231', 'cla-miR-50']:
+                # take the reverse complement of the cla hairpin
+                newfile.write(reverse_complement(clahairpin[clamirna]) + '\n')
+            else:
+                newfile.write(clahairpin[clamirna] + '\n')
+            newfile.close()
+            
+
+# all pairs of mature sequences have been manually inspected in a first run
+
+for crmmirna in crmmature:
+    # if mirna has ortholog in latens
+    if crmmirna in matureorthos:
+        # do not align miR-2225_5p, sequences do not align at all
+        if crmmirna != 'crm-miR-2225_5p':
+            # get cla ortholog
+            clamirna = matureorthos[crmmirna]
+            if clamirna in clamature:
+                # open file for writing
+                # get file from mirna name
+                newfile = open('Crm_Cla_miRNA_orthos/' + crmmirna + '_mature.fasta', 'w')
+                newfile.write('>' + crmmirna + '\n')
+                newfile.write(crmmature[crmmirna] + '\n')
+                # if crm mature is miR-39, align aopposite arms
+                if crmmirna == 'crm-miR-39_5p':
+                    newfile.write('>cla-miR-39_3p' + '\n')
+                    newfile.write(clamature['cla-miR-39_3p'] + '\n')
+                elif crmmirna == 'crm-miR-39_3p':
+                    newfile.write('>cla-miR-39_5p' + '\n')
+                    newfile.write(clamature['cla-miR-39_5p'] + '\n')
+                else:
+                    # write cla mirna sequence
+                    newfile.write('>' + clamirna + '\n')
+                    if clamirna in ['cla-miR-231_5p', 'cla-miR-50_3p', 'cla-miR-50_5p']:
+                        # take the reverse complement of the cla mature                
+                        newfile.write(reverse_complement(clamature[clamirna]) + '\n')
+                    else:
+                        newfile.write(clamature[clamirna] + '\n')
+                newfile.close()
+            
+
+# align premirnas and mature sequences
+run_tcoffee_noncoding('Crm_Cla_miRNA_orthos/')
+
+## t-coffee outputs are saved in the current directory so move outputfiles
+#os.system('mv CRE_PX356*UTR* ./Crm_Cla_miRNA_orthos/')
+## convert the t-coffee output to fasta format and save in text files
+#generate_fasta_from_tcoffee('./Crm_Cla_miRNA_orthos/')
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# align premirnas
 
 # align matures mirnas
 
